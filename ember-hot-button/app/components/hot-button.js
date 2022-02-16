014bd33636accb230ee810a4cbe6070012f2b077
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-02-16 00:14:09 -0800

    In progress

---
+++ ember-hot-button/app/components/hot-button.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class HotButtonComponent extends Component {
+  @tracked
+  count = 0;
+
+  @action
+  incrementClick() {
+    this.count = this.count + 1;
+  }
+}
