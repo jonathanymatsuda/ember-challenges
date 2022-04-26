86eefad57d1e885ffe61a4cef7cd60b0b9a8ef0f
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-04-25 17:04:41 -0700

    Adds ember-hot-button component

--- app/components/hot-button.js
+++ app/components/hot-button.js
@@ -6,6 +6,25 @@ export default class HotButtonComponent extends Component {
   @tracked
   count = 0;
 
+  @tracked
+  classColorUpdates = '';
+
   @action
-  handleClick() {}
+  handleClick() {
+    this.count++;
+    console.log(this.count);
+    if (this.count < 3) {
+      this.classColorUpdates = 'bg-inherit';
+    } else if (this.count < 6) {
+      this.classColorUpdates = 'bg-yellow-200';
+    } else if (this.count < 9) {
+      this.classColorUpdates = 'bg-yellow-400';
+    } else if (this.count < 12) {
+      this.classColorUpdates = 'bg-yellow-600';
+    } else if (this.count < 15) {
+      this.classColorUpdates = 'bg-red-700';
+    } else {
+      this.classColorUpdates = 'bg-blue-900';
+    }
+  }
 }
